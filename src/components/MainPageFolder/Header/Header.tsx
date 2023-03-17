11fc2e28deb04c9153e234857df0b7965ac680ef
cyberpaxan <igorl098765@gmail.com>
2023-03-17 16:54:33 +0500

    added dynamic scroll from header

--- src/components/MainPageFolder/Header/Header.tsx
+++ src/components/MainPageFolder/Header/Header.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import styles from './Header.module.scss';
 import cart from '../../../assets/images/cart.png';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+    function ScrollToTop() {
+        const { pathname } = useLocation();
+
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, [pathname]);
+
+        return null;
+    }
+
+    ScrollToTop();
+
     return (
         <div>
             <div className={styles.header}>
@@ -12,15 +26,50 @@ const Header = () => {
                         <h1>Виноградовъ</h1>
                     </Link>
                     <div className={styles.header__logoAndCart__headerContent}>
-                        <h2>О компании</h2>
+                        <Link
+                            to='/'
+                            style={{ textDecoration: 'none' }}
+                            onClick={() =>
+                                window.scrollTo({
+                                    top: 940,
+                                    behavior: 'smooth',
+                                })
+                            }
+                        >
+                            <h2>О компании</h2>
+                        </Link>
+
                         <Link
                             to='/catalog/honey/all'
                             style={{ textDecoration: 'none' }}
                         >
                             <h2>Каталог</h2>
                         </Link>
-                        <h2>Оплата и доставка</h2>
-                        <h2>Контакты</h2>
+                        <Link
+                            to='/'
+                            style={{ textDecoration: 'none' }}
+                            onClick={() =>
+                                window.scrollTo({
+                                    top: 4150,
+                                    behavior: 'smooth',
+                                })
+                            }
+                        >
+                            <h2>Оплата и доставка</h2>
+                        </Link>
+
+                        <Link
+                            to='/'
+                            style={{ textDecoration: 'none' }}
+                            onClick={() =>
+                                window.scrollTo({
+                                    top: 8295,
+                                    behavior: 'smooth',
+                                })
+                            }
+                        >
+                            <h2>Контакты</h2>
+                        </Link>
                     </div>
                     <Link to='/cart'>
                         <img src={cart} />
