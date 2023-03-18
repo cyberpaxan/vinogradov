15c44599a8ad9ae9f348b4ede0238f58b74ef2f2
cyberpaxan <igorl098765@gmail.com>
2023-03-18 09:50:25 +0500

    added dynamic scroll from footer

--- src/components/MainPageFolder/Footer/Footer.tsx
+++ src/components/MainPageFolder/Footer/Footer.tsx
@@ -3,17 +3,58 @@ import styles from './Footer.module.scss';
 import vk from '../../../assets/images/ContactsImg/vk.png';
 import tg from '../../../assets/images/ContactsImg/tg.png';
 import ok from '../../../assets/images/ContactsImg/ok.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div>
             <div className={styles.footer}>
                 <div className={styles.footer__text}>
-                    <h1>Виноградовъ</h1>
-                    <h2>О компании</h2>
-                    <h2>Каталог</h2>
-                    <h2>Оплата и доставка</h2>
-                    <h2>Контакты</h2>
+                    <Link to='/' style={{ textDecoration: 'none' }}>
+                        <h1>Виноградовъ</h1>
+                    </Link>
+                    <Link
+                        to='/'
+                        style={{ textDecoration: 'none' }}
+                        onClick={() =>
+                            window.scrollTo({
+                                top: 940,
+                                behavior: 'smooth',
+                            })
+                        }
+                    >
+                        <h2>О компании</h2>
+                    </Link>
+                    <Link
+                        to='/catalog/honey/all'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <h2>Каталог</h2>
+                    </Link>
+                    <Link
+                        to='/'
+                        style={{ textDecoration: 'none' }}
+                        onClick={() =>
+                            window.scrollTo({
+                                top: 4150,
+                                behavior: 'smooth',
+                            })
+                        }
+                    >
+                        <h2>Оплата и доставка</h2>
+                    </Link>
+                    <Link
+                        to='/'
+                        style={{ textDecoration: 'none' }}
+                        onClick={() =>
+                            window.scrollTo({
+                                top: 8295,
+                                behavior: 'smooth',
+                            })
+                        }
+                    >
+                        <h2>Контакты</h2>
+                    </Link>
                 </div>
                 <div className={styles.footer__icons}>
                     <img src={vk} />
